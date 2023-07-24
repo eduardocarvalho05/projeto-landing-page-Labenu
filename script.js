e5c4b22809497f2c86a65441b38b66b5c068f7f6
Eduardo <e.alves.rj@outlook.com>
2023-07-24 17:02:12 -0300

    adicionando footer e formulario com interação dom

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function menuShow() {
    }
 }
 /////////////////////////////////
+
+function formulario() {
+   let nome = document.getElementById("username").value;
+   let email = document.getElementById("email").value;
+   let telefone = document.getElementById("telefone").value;
+
+   // Verificando se os campos estão preenchidos
+   if (nome === "" || email === "" || telefone === "") {
+      alert("Por favor, preencha todos os campos do formulário.");
+      return false;
+   } else {
+      alert(`Usuário ${nome} cadastrado com sucesso! Agora é só aguardar que nossos vendedores entraram em contato!`)
+   }
+
+}
